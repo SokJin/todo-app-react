62f1f879b80eeeb9a1781906de403ec7c77cd651
SUKJIN <jin_soku@pj100.biz>
2025-06-11 14:13:45 +0900

    indexファイル修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import Template from '@/components/todo/Template';
-import Insert from '@/components/todo/Insert';
-import List from '@/components/todo/List';
+import Container from '@/components/todo/Container';
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <Template>
-          <Insert />
-          <List />
-        </Template>
-      </div>
+      <Container />
     </main>
   );
 }

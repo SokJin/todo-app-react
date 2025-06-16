1c919332806d0e9f0b869c32a8dbc345e30b91de
SUKJIN <jin_soku@pj100.biz>
2025-06-16 14:31:11 +0900

    tailwindcss 整理

--- src/components/todo/Template.tsx
+++ src/components/todo/Template.tsx
@@ -6,13 +6,11 @@ type Props = {
 
 export default function Template({ children }: Props) {
   return (
-    <div className="min-h-screen flex justify-center items-start pt-20 bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-96 max-w-full">
-        <header className="mb-6 pt-0">
-          <h1 className="text-3xl text-center font-bold">TODOLIST</h1>
-        </header>
-        {children}
-      </div>
+    <div className="min-h-screen flex flex-col justify-start items-center pt-20 p-8 rounded-lg shadow-lg w-full max-w-full">
+      <header className="mb-6 pt-0">
+        <h1 className="text-3xl text-center font-bold">TODOLIST</h1>
+      </header>
+      {children}
     </div>
   );
 }
